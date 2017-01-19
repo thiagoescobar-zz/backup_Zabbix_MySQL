b92c29fc6b7959b0d3fc5c14c21fcf9887aca600
root <root@zabbix.localdomain>
2017-01-19 17:51:32 -0200

    changed the default directory for temp and final files to be the current directory ($PWD).

--- backup.sh
+++ backup.sh
@@ -8,9 +8,9 @@
 DATE=`date +%Y-%m-%d`
 
 # Generates a .sql file with the backup of the zabbix Database
-mysqldump -u zabbix -p -x -e -B zabbix > /root/backup_Zabbix_MySQL/zabbix-$DATE.sql
+mysqldump -u zabbix -p -x -e -B zabbix > $PWD/zabbix-$DATE.sql
 
 # Uses tar to create a .tar.bz2 file compacting the backup
-tar -cjf /root/backup_Zabbix_MySQL/zabbix-$DATE.tar.bz2 -C /root/backup_Zabbix_MySQL zabbix-$DATE.sql --remove-files
+tar -cjf $PWD/zabbix-$DATE.tar.bz2 -C $PWD zabbix-$DATE.sql --remove-files
 
 exit 0
