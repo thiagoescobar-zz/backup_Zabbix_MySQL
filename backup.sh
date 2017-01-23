4901a5ffb147efd30f06915b1a65940154e8950a
Thiago Escobar <thiago.escobar@gmail.com>
2017-01-23 13:45:09 -0200

    Included options for custom mysql user (-u), custom database (-d) and unexpected option handling. Edited the README file to include the changes made in code.

--- backup.sh
+++ backup.sh
@@ -7,14 +7,37 @@
 
 DATE=`date +%Y-%m-%d`
 
-while getopts ":p:" opt; do
+while getopts ":p:d:u:" opt; do
 	case $opt in
 		p)
 			PASS=$OPTARG
+			;;
+		d)
+			DB=$OPTARG
+			;;
+		u)
+			DBUSER=$OPTARG
+			;;
+		\?)
+			echo "Invalid option: $OPTARG " >&2
+			echo "exiting now" >&2
+			exit 1
+			;;
 	esac
 done
+
+if [$DB = ''] ; then
+	DB='zabbix'
+	echo "DB setado para $DB"
+fi
+
+
+if [$DBUSER = ''] ; then
+	USER='zabbix'
+	echo "USER setado para $USER"
+fi
 # Generates a .sql file with the backup of the zabbix Database
-mysqldump -u zabbix -p$PASS -x -e -B zabbix > $PWD/zabbix-$DATE.sql
+mysqldump -u $USER -p$PASS -x -e -B $DB > $PWD/zabbix-$DATE.sql
 
 # Uses tar to create a .tar.bz2 file compacting the backup
 tar -cjf $PWD/zabbix-$DATE.tar.bz2 -C $PWD zabbix-$DATE.sql --remove-files
