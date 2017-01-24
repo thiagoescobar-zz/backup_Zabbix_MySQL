368a66bc800da1b20e8038e9c9217ebbee07b932
Thiago Escobar <thiago.escobar@gmail.com>
2017-01-24 15:29:07 -0200

    removed some debug commands . fixed the script to correctly use $DBUSER instead of $USER

--- backup.sh
+++ backup.sh
@@ -28,16 +28,14 @@ done
 
 if [$DB = ''] ; then
 	DB='zabbix'
-	echo "DB setado para $DB"
 fi
 
 
 if [$DBUSER = ''] ; then
-	USER='zabbix'
-	echo "USER setado para $USER"
+	DBUSER='zabbix'
 fi
 # Generates a .sql file with the backup of the zabbix Database
-mysqldump -u $USER -p$PASS -x -e -B $DB > $PWD/zabbix-$DATE.sql
+mysqldump -u $DBUSER -p$PASS -x -e -B $DB > $PWD/zabbix-$DATE.sql
 
 # Uses tar to create a .tar.bz2 file compacting the backup
 tar -cjf $PWD/zabbix-$DATE.tar.bz2 -C $PWD zabbix-$DATE.sql --remove-files
